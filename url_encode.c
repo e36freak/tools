7811965a37dabae2febf28691b930cff578f9c39
DJ Mills <danielmills1@gmail.com>
2012-03-02 02:17:16 -0500

    change url_encode to tokenize on "/" and escape each field separately

--- url_encode.c
+++ url_encode.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <curl/curl.h>
+#include <string.h>
+
+/* sane URL length limit */
+#define LEN 4096
 
 int main(int argc, char *argv[]) {
+  char *p, out[LEN], cur[LEN];
+  int i=0;
+
+  /* check to make sure an arg is provided */
   if (argc < 2) {
     fprintf(stderr, "%s\n", "no URL provided");
     return 1;
   }
 
-  printf("%s\n", curl_easy_escape(NULL, argv[1], 0));
+  /* tokenize on '/' */
+  for (p = strtok(argv[1], "/"); p; p = strtok(NULL, "/")) {
+    sprintf(cur, "%s", p);
+
+    /* for first iteration, do not escape, and append an extra '/' if it
+       ends with ':' to supply the protocol */
+    if (!i++) {
+      if (cur[strlen(cur) - 1] == ':') {
+        sprintf(out, "%s/", cur);
+      } else {
+        sprintf(out, "%s", p);
+      }
+
+      continue;
+    }
+
+    /* escape string, append to out */
+    sprintf(out, "%s/%s", out, curl_easy_escape(NULL, cur, 0));
+  }
+
+  printf("%s\n", out);
+
 
   return 0;
 }
