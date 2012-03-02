a49e85fd503b0a98b830909032b0581edee29e01
DJ Mills <danielmills1@gmail.com>
2012-03-01 19:29:35 -0500

    add check for args in url_encode.c

--- url_encode.c
+++ url_encode.c
@@ -2,6 +2,11 @@
 #include <curl/curl.h>
 
 int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    fprintf(stderr, "%s\n", "no URL provided");
+    return 1;
+  }
+
   printf("%s\n", curl_easy_escape(NULL, argv[1], 0));
 
   return 0;
