479b2a8aa0e5907db31877e6b0cec25aaf8814f0
DJ Mills <danielmills1@gmail.com>
2012-04-09 13:20:45 -0400

    fix args.c to handle 0 args

--- args.c
+++ args.c
@@ -4,10 +4,11 @@
 int main(int argc, char *argv[]) {
   int i;
 
-  printf("%d arg%s: ", argc - 1, argc > 2 ? "s" : "");
+  printf("%d arg%s:", argc - 1, argc == 2 ? "" : "s");
   for (i=1; i<argc; i++) {
-    printf((i==argc-1)?"<%s>\n":"<%s> ", argv[i]);
+    printf(" <%s>", argv[i]);
   }
+  printf("\n");
 
   return 0;
 }
