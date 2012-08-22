7137efbdafbc52bc1157e6eba2964281cec79e9b
DJ Mills <danielmills1@gmail.com>
2012-08-22 13:03:26 -0400

    Revert "improved error handling"
    This reverts commit c9a7988f31c7f6a934852e5c1712c388df3eb374.

--- getmtime.c
+++ getmtime.c
@@ -2,35 +2,28 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 
 int main(int argc, char *argv[]) {
   struct stat st;
-  char mtime[512], err[128];
+  char mtime[128];
   int i;
 
   if (argc < 3) {
     fprintf(stderr, "usage: %s FORMAT FILE [...]\n\n", argv[0]);
     fprintf(stderr, "prints the mtime for each FILE given according to FORMAT\n");
     fprintf(stderr, "FORMAT is any string valid for strftime(3)\n");
-//    fprintf(stderr, "%%N may also be used for the filename\n");
 
-    exit(EXIT_FAILURE);
+    return 1;
   }
 
   for (i=2; i<argc; i++) {
     if (stat(argv[i], &st) == -1) {
-      sprintf(err, "%s: %s", argv[0], argv[i]);
-      perror(err);
-      exit(EXIT_FAILURE);
+      perror(argv[0]);
+      return 1;
     }
 
-    if (strftime(mtime, sizeof(mtime), argv[1], localtime(&st.st_mtime)) == 0) {
-      fprintf(stderr, "%s: %s: strftime error\n", argv[0], argv[i]);
-      exit(EXIT_FAILURE);
-    }
-
+    strftime(mtime, sizeof(mtime), argv[1], localtime(&st.st_mtime));
     printf("%s\n", mtime);
   }
 
