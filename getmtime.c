f242658f2e8dc928e891f92efe213cd73f221afb
DJ Mills <danielmills1@gmail.com>
2012-08-22 13:06:01 -0400

    improve error handling

--- getmtime.c
+++ getmtime.c
@@ -1,12 +1,13 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 
 int main(int argc, char *argv[]) {
   struct stat st;
-  char mtime[128];
+  char mtime[512], err[512];
   int i;
 
   if (argc < 3) {
@@ -14,16 +15,23 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "prints the mtime for each FILE given according to FORMAT\n");
     fprintf(stderr, "FORMAT is any string valid for strftime(3)\n");
 
-    return 1;
+    exit(EXIT_FAILURE);
   }
 
   for (i=2; i<argc; i++) {
     if (stat(argv[i], &st) == -1) {
-      perror(argv[0]);
-      return 1;
+      sprintf(err, "%s: %s", argv[0], argv[i]);
+      perror(err);
+
+      exit(EXIT_FAILURE);
     }
 
-    strftime(mtime, sizeof(mtime), argv[1], localtime(&st.st_mtime));
+    if (strftime(mtime, sizeof(mtime), argv[1], localtime(&st.st_mtime)) == 0) {
+      fprintf("%s: %s: strftime error\n", argv[0], argv[i]);
+
+      exit(EXIT_FAILURE);
+    }
+
     printf("%s\n", mtime);
   }
 
